b8ed1dc38b4477824b1b8953b8d2311c1629d105
userwei <70355885+chou-ting-wei@users.noreply.github.com>
2024-09-06 01:47:19 +0800

    add week01

---
+++ week01/W1B1-Grade Sorting - Simplified.cpp
@@ -0,0 +1,136 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by userwei
+//
+
+#ifdef LOCAL
+    #include "/Users/chou.ting.wei/Coding/stdc++.h"
+#else
+    #include <bits/stdc++.h>
+    #include <bits/extc++.h>
+    #pragma GCC optimize("O3,unroll-loops")
+
+    using namespace __gnu_pbds;
+#endif
+
+using namespace std;
+ 
+#define int long long
+#define ull unsigned long long
+
+#define For(z, x, y) for(int z = x; z <= y; z ++)
+#define Forr(z, x, y) for(int z = x; z >= y; z --)
+#define lowbit(x) ((x) & -(x))
+ 
+#define ef emplace_front
+#define eb emplace_back
+#define all(x) x.begin(), x.end()
+#define sz(x) ((int) x.size())
+#define rev(x) reverse(all(x))
+ 
+#define mp make_pair
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define F first
+#define S second
+ 
+#define endl '\n'
+ 
+template<typename T> inline void chmin(T &_a, const T &_b) { if(_b < _a) _a = _b; }
+template<typename T> inline void chmax(T &_a, const T &_b) { if(_b > _a) _a = _b; }
+ 
+clock_t start;
+ 
+void getAC(){
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);
+        start = clock();
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+ 
+void time(){
+    #ifdef LOCAL
+        cerr << "Execution Time : " << double(clock() - start) / CLOCKS_PER_SEC << " (s)" << endl;
+        fclose(stdin), fclose(stdout), fclose(stderr);
+    #endif
+}
+ 
+int max(int a, int b) { return a > b ? a : b; }
+int gcd(int a, int b) { if(a == 0) return b; return b == 0 ? a : gcd(b, a % b); }
+ 
+void YES(bool b) { cout << (b ? "YES" : "NO") << endl; }
+void Yes(bool b) { cout << (b ? "Yes" : "No") << endl; }
+void sto(int k, int m) { cout << (k ? " " : "") << m; }
+ 
+const double PI = acos(-1);
+const int INF = 1e18;
+ 
+struct custom_hash{
+    // unordered_map<int, int, custom_hash> safe_map;
+    // gp_hash_table<int, int, custom_hash> safe_hash_table;
+    static uint64_t splitmix64(uint64_t x){
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const{
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+struct stu{
+    int sc[10];
+    int sum = 0;
+    int over = 0;
+}s[100005];
+
+bool cmp(stu &a, stu &b){
+    if(a.over != b.over) return a.over > b.over;
+    if(a.sum != b.sum) return a.sum > b.sum;
+    if(a.sc[1] != b.sc[1]) return a.sc[1] > b.sc[1];
+    if(a.sc[2] != b.sc[2]) return a.sc[2] > b.sc[2];
+    if(a.sc[3] != b.sc[3]) return a.sc[3] > b.sc[3];
+    if(a.sc[4] != b.sc[4]) return a.sc[4] > b.sc[4];
+    return a.sc[5] > b.sc[5];
+}
+
+int32_t main(){
+    getAC();
+
+    int n;
+    cin >> n;
+    For(i, 1, n){
+        int tmp = 0, tmp2 = 1;
+        For(j, 1, 5){
+            int k;
+            cin >> k;
+            s[i].sc[j] = k;
+            tmp += k;
+            if(k < 90) tmp2 = 0;
+        }
+        s[i].sum = tmp;
+        s[i].over = tmp2;
+    }
+
+    sort(s+1, s+n+1, cmp);
+
+    For(i, 1, n){
+        For(j, 1, 5){
+            cout << s[i].sc[j] << " \n"[j == 5];
+        }
+    }
+
+    time();
+    return 0;
+}
---
+++ week01/W1C2-Vector Sorting.cpp
@@ -0,0 +1,160 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by userwei
+//
+
+#ifdef LOCAL
+    #include "/Users/chou.ting.wei/Coding/stdc++.h"
+#else
+    #include <bits/stdc++.h>
+    #include <bits/extc++.h>
+    #pragma GCC optimize("O3,unroll-loops")
+
+    using namespace __gnu_pbds;
+#endif
+
+using namespace std;
+ 
+#define int long long
+#define ull unsigned long long
+
+#define For(z, x, y) for(int z = x; z <= y; z ++)
+#define Forr(z, x, y) for(int z = x; z >= y; z --)
+#define lowbit(x) ((x) & -(x))
+ 
+#define ef emplace_front
+#define eb emplace_back
+#define all(x) x.begin(), x.end()
+#define sz(x) ((int) x.size())
+#define rev(x) reverse(all(x))
+ 
+#define mp make_pair
+#define pii pair<int, int>
+#define pdd pair<double, double>
+// #define F first
+// #define S second
+ 
+#define endl '\n'
+ 
+template<typename T> inline void chmin(T &_a, const T &_b) { if(_b < _a) _a = _b; }
+template<typename T> inline void chmax(T &_a, const T &_b) { if(_b > _a) _a = _b; }
+ 
+clock_t start;
+ 
+void getAC(){
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);
+        start = clock();
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+ 
+void time(){
+    #ifdef LOCAL
+        cerr << "Execution Time : " << double(clock() - start) / CLOCKS_PER_SEC << " (s)" << endl;
+        fclose(stdin), fclose(stdout), fclose(stderr);
+    #endif
+}
+ 
+int max(int a, int b) { return a > b ? a : b; }
+int gcd(int a, int b) { if(a == 0) return b; return b == 0 ? a : gcd(b, a % b); }
+ 
+void YES(bool b) { cout << (b ? "YES" : "NO") << endl; }
+void Yes(bool b) { cout << (b ? "Yes" : "No") << endl; }
+void sto(int k, int m) { cout << (k ? " " : "") << m; }
+ 
+const double PI = acos(-1);
+const int INF = 1e18;
+ 
+struct custom_hash{
+    // unordered_map<int, int, custom_hash> safe_map;
+    // gp_hash_table<int, int, custom_hash> safe_hash_table;
+    static uint64_t splitmix64(uint64_t x){
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const{
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define X first
+#define Y second
+
+pii operator+(pii a, pii b){
+    return pii(a.X + b.X, a.Y + b.Y);
+}
+pii operator-(pii a, pii b){
+    return pii(a.X - b.X, a.Y - b.Y);
+}
+pii operator*(pii a, int b){
+    return pii(a.X * b, a.Y * b);
+}
+
+int dot(pii a, pii b){
+    return a.X * b.X + a.Y * b.Y;
+}
+int cross(pii a, pii b){
+    return a.X * b.Y - a.Y * b.X;
+}
+int abs2(pii a){
+    return dot(a, a);
+}
+
+int sign(int a){
+    return a == 0 ? 0 : a > 0 ? 1 : -1;
+}
+int ori(pii a, pii b, pii c){
+    return sign(cross(b - a, c - a));
+}
+
+struct Pt{
+    pii cor;
+    int idx;
+}pt[100005];
+
+int maxx = 0, maxidx = 0;
+pii maxcor;
+
+bool cmp(Pt &a, Pt &b){
+    if(ori(maxcor, a.cor, b.cor) != 0) return ori(maxcor, a.cor, b.cor) > 0;
+    // cerr << a.idx << " " << b.idx << " " << abs2(a.cor - maxcor) << " " << abs2(b.cor - maxcor) << endl;
+    return abs2(a.cor - maxcor) < abs2(b.cor - maxcor);
+}
+
+int32_t main(){
+    getAC();
+    
+    int n;
+    cin >> n;
+
+    For(i, 1, n){
+        cin >> pt[i].cor.X >> pt[i].cor.Y;
+        pt[i].idx = i;
+        if(pt[i].cor.X > maxx){
+            maxx = pt[i].cor.X;
+            maxidx = i;
+            maxcor = pt[i].cor;
+        }
+    }
+
+    sort(pt+1, pt+n+1, cmp);
+
+    For(i, 1, n){
+        cout << pt[i].idx << " \n"[i==n];
+    }
+
+    time();
+    return 0;
+}
